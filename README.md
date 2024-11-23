d2051d51d1cd9803eeaa4d64824883af3469da06
emad-Asus <emadg.dev@gmail.com>
2024-11-23 16:02:11 +0330

    architecture altered

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
 superuser:
 username: admin
-password: 1899@Roshan
+password: 1899@api
 
-
-{
-    "token": "c9442607ef49effb7426f26d187766a34ccd33f0",
-    "user": {
-        "id": 3,
-        "username": "emad",
-        "password": "18991899",
-        "email": "a@sarvfaraz.com"
-    }
-}
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,25 @@
-from django.urls import path
-from .views import *
+"""
+URL configuration for api project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from authentication import views
 
 urlpatterns = [
-    path('auth/login/', login, name='login'),
-    path('auth/token/', post_token, name='post_token'),
-    path('auth/token/logout/', logout, name='logout'),
-    path('auth/signup/', signup, name='signup'),
-    path('home/', home, name='home'),
+    path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls')),
+    path('', views.home)
 ]
